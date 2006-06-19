e290c68b833a3fa2ecfe2c89fa375d4df52d9733
Florian Wesch <fw@dividuum.de>
2006-06-19 22:12:32 +0000

    Test 'GUI' Client
    git-svn-id: http://infon.googlecode.com/svn/trunk@8 8171fb75-e542-0410-96e4-03d5dd800671

---
+++ client.rb
@@ -0,0 +1,25 @@
+require 'socket'
+TCPSocket.open('localhost', 1234) { |socket|
+    p socket.gets
+    socket.write("g\n")
+    while foo = socket.read(1) 
+        case foo[0]
+        when 0:
+            print "player static: "
+            print "pno=%d " % socket.read(1)[0]
+            print "name=%s " % socket.read(32).unpack("Z*")[0]
+            puts  "color=%d" % socket.read(1)[0]
+        when 1:
+            print "player round: "
+            print "pno=%d " % socket.read(1)[0]
+            print "score=%d " % socket.read(4).unpack("N")[0]
+            puts "cycles=%d" % socket.read(4).unpack("N")[0]
+        when 2:
+            print "player join/leave: "
+            print "pno=%d " % socket.read(1)[0]
+            puts "op=%s" % (socket.read(1)[0] == 0 ? "leave" : "join")
+        else
+            print "???"
+        end
+    end
+}
