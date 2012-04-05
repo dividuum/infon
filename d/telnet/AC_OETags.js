7dd128c4f04013762f308aed21cd846adf4bce3d
Florian Wesch <fw@dividuum.de>
2012-04-05 16:25:30 +0200

    migrate webpage to gh-pages

---
+++ d/telnet/AC_OETags.js
@@ -0,0 +1,163 @@
+// v1.5
+// Detect Client Browser type
+var isIE  = (navigator.appVersion.indexOf("MSIE") != -1) ? true : false;
+var isWin = (navigator.appVersion.toLowerCase().indexOf("win") != -1) ? true : false;
+var isOpera = (navigator.userAgent.indexOf("Opera") != -1) ? true : false;
+
+// JavaScript helper required to detect Flash Player PlugIn version information
+function JSGetSwfVer(i){
+	// NS/Opera version >= 3 check for Flash plugin in plugin array
+	var flashVer = -1;
+	if (navigator.plugins != null && navigator.plugins.length > 0) {
+		if (navigator.plugins["Shockwave Flash 2.0"] || navigator.plugins["Shockwave Flash"]) {
+			var swVer2 = navigator.plugins["Shockwave Flash 2.0"] ? " 2.0" : "";
+      		var flashDescription = navigator.plugins["Shockwave Flash" + swVer2].description;
+			var descArray = flashDescription.split(" ");
+			var tempArrayMajor = descArray[2].split(".");
+			var versionMajor = tempArrayMajor[0];
+			var versionMinor = tempArrayMajor[1];
+			if ( descArray[3] != "" ) {
+				tempArrayMinor = descArray[3].split("r");
+			} else {
+				tempArrayMinor = descArray[4].split("r");
+			}
+      		var versionRevision = tempArrayMinor[1] > 0 ? tempArrayMinor[1] : 0;
+            var flashVer = versionMajor + "." + versionMinor + "." + versionRevision;
+      	}
+	}
+	// MSN/WebTV 2.6 supports Flash 4
+	else if (navigator.userAgent.toLowerCase().indexOf("webtv/2.6") != -1) flashVer = 4;
+	// WebTV 2.5 supports Flash 3
+	else if (navigator.userAgent.toLowerCase().indexOf("webtv/2.5") != -1) flashVer = 3;
+	// older WebTV supports Flash 2
+	else if (navigator.userAgent.toLowerCase().indexOf("webtv") != -1) flashVer = 2;
+
+	return flashVer;
+}
+
+// When called with reqMajorVer, reqMinorVer, reqRevision returns true if that version or greater is available
+function DetectFlashVer(reqMajorVer, reqMinorVer, reqRevision)
+{
+ 	reqVer = parseFloat(reqMajorVer + "." + reqRevision);
+   	// loop backwards through the versions until we find the newest version
+	for (i=25;i>0;i--) {
+		if (isIE && isWin && !isOpera) {
+			versionStr = VBGetSwfVer(i);
+		} else {
+			versionStr = JSGetSwfVer(i);
+		}
+		if (versionStr == -1 ) {
+			return false;
+		} else if (versionStr != 0) {
+			if(isIE && isWin && !isOpera) {
+				tempArray         = versionStr.split(" ");
+				tempString        = tempArray[1];
+				versionArray      = tempString .split(",");
+			} else {
+				versionArray      = versionStr.split(".");
+			}
+			var versionMajor      = versionArray[0];
+			var versionMinor      = versionArray[1];
+			var versionRevision   = versionArray[2];
+
+			var versionString     = versionMajor + "." + versionRevision;   // 7.0r24 == 7.24
+			var versionNum        = parseFloat(versionString);
+        	// is the major.revision >= requested major.revision AND the minor version >= requested minor
+			if (versionMajor > reqMajorVer) {
+				return true;
+			} else if (versionMajor == reqMajorVer) {
+				if (versionMinor > reqMinorVer)
+					return true;
+				else if (versionMinor == reqMinorVer) {
+					if (versionRevision >= reqRevision)
+						return true;
+				}
+			}
+			return false;
+		}
+	}
+}
+
+function AC_AddExtension(src, ext)
+{
+  if (src.indexOf('?') != -1)
+    return src.replace(/\?/, ext+'?'); 
+  else
+    return src + ext;
+}
+
+function AC_Generateobj(objAttrs, params, embedAttrs) 
+{ 
+  var str = '<object ';
+  for (var i in objAttrs)
+    str += i + '="' + objAttrs[i] + '" ';
+  str += '>';
+  for (var i in params)
+    str += '<param name="' + i + '" value="' + params[i] + '" /> ';
+  str += '<embed ';
+  for (var i in embedAttrs)
+    str += i + '="' + embedAttrs[i] + '" ';
+  str += ' ></embed></object>';
+
+  document.write(str);
+}
+
+//v1.0
+function AC_FL_RunContent(){
+  var ret = 
+    AC_GetArgs
+    (  arguments, ".swf", "movie", "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+     , "http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version="
+     , "http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"
+     , "application/x-shockwave-flash", "6,0,0,0"
+    );
+  AC_Generateobj(ret.objAttrs, ret.params, ret.embedAttrs);
+}
+
+function AC_GetArgs(args, ext, srcParamName, classid, codebase, pluginsPage, mimeType, cbVers){
+  var ret = new Object();
+  ret.embedAttrs = new Object();
+  ret.params = new Object();
+  ret.objAttrs = new Object();
+  for (var i=0; i < args.length; i=i+2){
+    var currArg = args[i].toLowerCase();    
+
+    switch (currArg){	
+      case "codebase":
+      case "pluginspage":
+      case "type":
+      case "classid":
+        break;
+      case "src":
+      case "movie":	
+        args[i+1] = AC_AddExtension(args[i+1], ext);
+        ret.embedAttrs["src"] = args[i+1];
+        ret.params[srcParamName] = args[i+1];
+        break;
+      case "minversion":
+        cbVers = args[i+1];
+        break;
+      case "width":
+      case "height":
+      case "align":
+      case "vspace": 
+      case "hspace":
+      case "class":
+      case "title":
+      case "accesskey":
+      case "name":
+      case "id":
+      case "tabindex":
+        ret.embedAttrs[args[i]] = ret.objAttrs[args[i]] = args[i+1];
+        break;
+      default:
+        ret.embedAttrs[args[i]] = ret.params[args[i]] = args[i+1];
+    }
+  }
+  ret.objAttrs["classid"] = classid;
+  ret.objAttrs["codebase"] = codebase + cbVers;
+  ret.embedAttrs["pluginspage"] = pluginsPage;
+  if (mimeType) ret.embedAttrs["type"] = mimeType;
+  return ret;
+}
+
